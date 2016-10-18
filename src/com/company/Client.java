29767d24a2ae7440f01e7a623497a5fc094f966f
vitekxl <17123vitek@gmail.com>
2016-10-18 03:37:21 +0300

    rewrite for threads

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -19,43 +19,52 @@ public class Client {
 
     public boolean connect()
     {
-        // Здесь указан адрес того самого компьютера где будет исполняться и клиент.
 
         try {
-            InetAddress ipAddress = InetAddress.getByName(serverIP); // создаем объект который отображает вышеописанный serverIP-адрес.
-            System.out.println("Any of you heard of a socket with serverIP address " + serverIP + " and serverPort " + serverPort + "?");
-            Socket socket = new Socket(ipAddress, serverPort); // создаем сокет используя serverIP-адрес и порт сервера.
-            System.out.println("Yes! I just got hold of the program.");
+            System.out.println("try connect to ip=" + serverIP + " port=" +serverPort);
+            InetAddress ipAddress = InetAddress.getByName(serverIP);
+            socket = new Socket(ipAddress, serverPort);
+            System.out.println("Connected to server!");
 
-            // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиентом.
             InputStream sin = socket.getInputStream();
-            OutputStream sout = socket.getOutputStream();
-
-            // Конвертируем потоки в другой тип, чтоб легче обрабатывать текстовые сообщения.
             DataInputStream in = new DataInputStream(sin);
-            DataOutputStream out = new DataOutputStream(sout);
+            String line= "";
 
-            // Создаем поток для чтения с клавиатуры.
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-            String line = null;
-            System.out.println("Type in something and press enter. Will send it to the server and tell ya what it thinks.");
-            System.out.println();
+            new Thread(new WriteMessage()).start();
 
             while (true) {
-                line = keyboard.readLine(); // ждем пока пользователь введет что-то и нажмет кнопку Enter.
-                System.out.println("Sending this line to the server...");
-                out.writeUTF(line); // отсылаем введенную строку текста серверу.
-                out.flush(); // заставляем поток закончить передачу данных.
                 line = in.readUTF(); // ждем пока сервер отошлет строку текста.
-                System.out.println("The server was very polite. It sent me this : " + line);
-                System.out.println("Looks like the server is pleased with us. Go ahead and enter more lines.");
-                System.out.println();
-                break;
+                System.out.println(line);
             }
         } catch (Exception x) {
             x.printStackTrace();
             return false;
         }
-        return true;
+    }
+
+    private void writeMessageThr()
+    {
+        try {
+            OutputStream sout = socket.getOutputStream();
+            DataOutputStream out = new DataOutputStream(sout);
+            String line = "";
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            while(true) {
+                line = keyboard.readLine();
+                System.out.println("Sending this line to the server...");
+                out.writeUTF(line);
+                out.flush();
+            }
+        }
+        catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+
+    class WriteMessage implements Runnable{
+        @Override
+        public void run() {
+            writeMessageThr();
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //todo error when server disconnected
 	// write your code here
-        Client client = new Client("127.0.0.1", 4444);
+        int port;
+        if(args.length > 0)
+             port = Integer.parseInt(args[0]);
+        else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter port");
+            port = Integer.parseInt(scanner.nextLine());
+        }
+        Client client = new Client("127.0.0.1", port);
         client.connect();
     }
 }
